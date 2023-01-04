e8de7044f14487b79fd7f87dabedd84b9369911d
Vitalii Sandul <vitalii.sandul@ringteam.com>
2023-01-04 11:11:50 +0200

    commit #3 binding name-password-salary

--- Neco_courses/lesson_9_view_binding/app/src/main/java/com/sandul/lesson_9_view_binding/MainActivity.kt
+++ Neco_courses/lesson_9_view_binding/app/src/main/java/com/sandul/lesson_9_view_binding/MainActivity.kt
@@ -2,34 +2,49 @@ package com.sandul.lesson_9_view_binding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.sandul.lesson_9_view_binding.constants.Constants
 import com.sandul.lesson_9_view_binding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
-    val a = 324
-    val b = 34
-    val currentPerson = 95
-    val maxPerson = 90
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.buttonPlus.setOnClickListener {
-            binding.tvResult.text = when (currentPerson) {
-                !in 0..maxPerson -> "Limit exceeded"
-                else -> "The limit is not exceeded"
+        binding.btnResult.setOnClickListener {
+            val editText = binding.edValue.text.toString()
+            val editPass = binding.edPassword.text.toString().toInt()
+            var tmp = ""
+
+            when (editText) {
+                Constants.DIRECTOR -> {
+                    if (editPass == Constants.DIRECTOR_PASSWORD) {
+                        tmp = "${Constants.DIRECTOR}, your salary is ${Constants.DIRECTOR_SALARY} USD/month"
+                        binding.tvResult.text = tmp
+                    }
+                }
+                Constants.IOS_ENGINEER -> {
+                    if (editPass == Constants.IOS_ENGINEER_PASSWORD) {
+                        tmp = "${Constants.IOS_ENGINEER}, your salary is ${Constants.IOS_ENGINEER_SALARY} USD/month"
+                        binding.tvResult.text = tmp
+                    }
+                }
+                Constants.ANDROID_ENGINEER -> {
+                    if (editPass == Constants.ANDROID_ENGINEER_PASSWORD) {
+                        tmp = "${Constants.ANDROID_ENGINEER}, your salary is ${Constants.ANDROID_ENGINEER_SALARY} USD/month"
+                        binding.tvResult.text = tmp
+                    }
+                }
+                else -> {
+                    tmp = "Wrong name or password"
+                    binding.tvResult.text = tmp
+                }
             }
-        }
-
-        binding.buttonMinus.setOnClickListener {
-            binding.tvResult.text = (a - b).toString()
-        }
-
-        binding.buttonMultiply.setOnClickListener {
-            binding.tvResult.text = (a * b).toString()
+            binding.tvResult.visibility = View.VISIBLE
         }
     }
 }
---
+++ Neco_courses/lesson_9_view_binding/app/src/main/java/com/sandul/lesson_9_view_binding/constants/Constants.kt
@@ -0,0 +1,15 @@
+package com.sandul.lesson_9_view_binding.constants
+
+object Constants {
+    const val DIRECTOR_SALARY = 50000
+    const val IOS_ENGINEER_SALARY = 20000
+    const val ANDROID_ENGINEER_SALARY = 20000
+
+    const val DIRECTOR = "Sergiy"
+    const val IOS_ENGINEER = "Alex"
+    const val ANDROID_ENGINEER = "Vitalii"
+
+    const val DIRECTOR_PASSWORD = 1234
+    const val IOS_ENGINEER_PASSWORD = 5678
+    const val ANDROID_ENGINEER_PASSWORD = 9012
+}
