3e4c3c5b985f3017628dbff1de79eca821450017
Vitalii Sandul <vitalii.sandul@ringteam.com>
2023-04-13 12:48:41 +0300

    DrawerLayout

--- Neco_courses/DrawerLayoutLesson/app/src/main/java/com/sandul/drawerlayoutlesson/MainActivity.kt
+++ Neco_courses/DrawerLayoutLesson/app/src/main/java/com/sandul/drawerlayoutlesson/MainActivity.kt
@@ -2,10 +2,22 @@ package com.sandul.drawerlayoutlesson
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.GravityCompat
+import com.sandul.drawerlayoutlesson.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            open.setOnClickListener {
+                drawer.openDrawer(GravityCompat.START)
+            }
+        }
     }
 }
