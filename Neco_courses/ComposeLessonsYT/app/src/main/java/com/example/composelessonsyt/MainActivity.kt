0c600f4e5d6404d390e50211103852d3c604c644
Vitalii Sandul <vitalii.sandul@ringteam.com>
2023-07-31 18:37:44 +0300

    Compose Lesson #3

--- Neco_courses/ComposeLessonsYT/app/src/main/java/com/example/composelessonsyt/MainActivity.kt
+++ Neco_courses/ComposeLessonsYT/app/src/main/java/com/example/composelessonsyt/MainActivity.kt
@@ -3,55 +3,73 @@ package com.example.composelessonsyt
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.composelessonsyt.ui.theme.ComposeLessonsYTTheme
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            Column(){
+                listItem("Will Smith - 1", "actor")
+                listItem("Will Smith - 2", "actor")
+                listItem("Will Smith - 3", "actor")
+                listItem("Will Smith - 4", "actor")
+            }
 
+        }
+    }
+}
+
+@Composable
+private fun listItem(name: String, prof: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        shape = RoundedCornerShape(15.dp),
+        elevation = CardDefaults.cardElevation(5.dp)
+
+    ) {
+        Box() {
             Row(
-                modifier = Modifier
-                    .background(Color.Gray)
-                    .fillMaxWidth().fillMaxHeight(0.5f),
-                horizontalArrangement = Arrangement.SpaceEvenly,
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                Column(
-                    modifier = Modifier.background(Color.Red).fillMaxWidth(0.5f).fillMaxHeight(0.5f),
-                    verticalArrangement = Arrangement.SpaceEvenly,
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Text(text = "Kyiv")
-                    Text(text = "Lviv")
-                    Text(text = "Odessa")
+                Image(
+                    painter = painterResource(id = R.drawable.will_smith),
+                    contentDescription = "image",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .size(64.dp)
+                        .clip(CircleShape)
+                )
+                Column(modifier = Modifier.padding(start = 10.dp)) {
+                    Text(text = name)
+                    Text(text = prof)
                 }
 
-                Column(
-                    modifier = Modifier.background(Color.Green).fillMaxHeight(0.75f),
-                    verticalArrangement = Arrangement.SpaceEvenly,
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Text(text = "Kyiv")
-                    Text(text = "Lviv")
-                    Text(text = "Odessa")
-                }
             }
         }
     }
 }
+
