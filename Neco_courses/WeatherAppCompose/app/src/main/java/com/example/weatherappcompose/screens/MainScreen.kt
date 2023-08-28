84c3cf4525c635d80da630be90a5e3b6c85ba61d
Vitalii Sandul <vitalii.sandul@ringteam.com>
2023-08-28 18:12:25 +0300

    Compose Lesson #11 weather app practice - IconButton

--- Neco_courses/WeatherAppCompose/app/src/main/java/com/example/weatherappcompose/screens/MainScreen.kt
+++ Neco_courses/WeatherAppCompose/app/src/main/java/com/example/weatherappcompose/screens/MainScreen.kt
@@ -11,6 +11,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItemDefaults.Elevation
 import androidx.compose.material3.LocalAbsoluteTonalElevation
 import androidx.compose.material3.NavigationBarDefaults.Elevation
@@ -47,6 +49,7 @@ fun MainScreen() {
         modifier = Modifier
             .fillMaxSize()
             .padding(5.dp)
+            .alpha(0.7f)
     ) {
         Card(
             modifier = Modifier
@@ -58,24 +61,78 @@ fun MainScreen() {
                 modifier = Modifier.fillMaxWidth(),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceBetween
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+
                 ) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(
+                            modifier = Modifier.padding(top = 3.dp, start = 8.dp),
+                            text = "20 June 2023 13:00",
+                            style = TextStyle(fontSize = 15.sp),
+                            color = Color.White
+                        )
+                        AsyncImage(
+                            model = "https://cdn.weatherapi.com/weather/64x64/day/116.png",
+                            contentDescription = "Image_2",
+                            modifier = Modifier
+                                .size(35.dp)
+                                .padding(top = 8.dp, end = 8.dp)
+                        )
+                    }
                     Text(
-                        modifier = Modifier.padding(top = 3.dp, start = 8.dp),
-                        text = "20 June 2023 13:00",
-                        style = TextStyle(fontSize = 15.sp),
+                        text = "Kiev",
+                        style = TextStyle(fontSize = 24.sp),
                         color = Color.White
                     )
-                    AsyncImage(
-                        model = "https://cdn.weatherapi.com/weather/64x64/day/116.png",
-                        contentDescription = "Image_2",
-                        modifier = Modifier.size(35.dp)
-                            .padding(top = 8.dp, end = 8.dp)
+                    Text(
+                        text = "23°C",
+                        style = TextStyle(fontSize = 65.sp),
+                        color = Color.White
                     )
+                    Text(
+                        text = "Sunny",
+                        style = TextStyle(fontSize = 16.sp),
+                        color = Color.White
+                    )
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        IconButton(
+                            onClick = {
+
+                            }) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_search),
+                                contentDescription = "Image_3",
+                                tint = Color.White
+                            )
+                        }
+                        Text(
+                            modifier = Modifier.padding(top = 3.dp, start = 8.dp),
+                            text = "25°C / 7°C",
+                            style = TextStyle(fontSize = 16.sp),
+                            color = Color.White
+                        )
+                        IconButton(
+                            onClick = {
+
+                            }) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_cloud_sync),
+                                contentDescription = "Image_4",
+                                tint = Color.White
+                            )
+                        }
+                    }
                 }
+
             }
         }
     }
